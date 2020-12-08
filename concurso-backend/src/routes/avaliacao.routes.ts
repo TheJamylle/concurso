6411a13ecb637501b4df03b2ebb43de89f34d147
TheJamylle <jamyllemagal@gmail.com>
2020-12-08 15:42:51 -0300

    Routes Avaliacao

--- concurso-backend/src/routes/avaliacao.routes.ts
+++ concurso-backend/src/routes/avaliacao.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import AvaliacaoService from '../services/AvaliacaoService';
 
 const avaliacaoRouter = Router();
 
 avaliacaoRouter.post('/', async (request, response) => {
   try {
-      
+    const { parecer, nota, data_avaliacao, id_avaliador, id_projeto } = request.body;
+
+    const avaliacao = await new AvaliacaoService().create({ parecer, nota, data_avaliacao, id_avaliador, id_projeto });
+
+    return response.json(avaliacao);
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
@@ -12,7 +17,9 @@ avaliacaoRouter.post('/', async (request, response) => {
 
 avaliacaoRouter.get('/', async (request, response) => {
   try {
-      
+    const avaliacoes = await new AvaliacaoService().list();
+
+    return response.json(avaliacoes);
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
@@ -20,7 +27,11 @@ avaliacaoRouter.get('/', async (request, response) => {
 
 avaliacaoRouter.get('/:id', async (request, response) => {
   try {
-      
+    const id_avaliacao = request.params.id;
+
+    const avaliacao = await new AvaliacaoService().getByID(id_avaliacao);
+
+    return avaliacao;
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
@@ -28,7 +39,13 @@ avaliacaoRouter.get('/:id', async (request, response) => {
 
 avaliacaoRouter.put('/:id', async (request, response) => {
   try {
-      
+    const id_avaliacao = request.params.id;
+    
+    const { parecer, nota, data_avaliacao, id_avaliador, id_projeto } = request.body;
+
+    const avaliacaoUpdated = await new AvaliacaoService().update({ parecer, nota, data_avaliacao, id_avaliador, id_projeto }, id_avaliacao);
+
+    return response.json(avaliacaoUpdated);
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
@@ -36,7 +53,11 @@ avaliacaoRouter.put('/:id', async (request, response) => {
 
 avaliacaoRouter.delete('/:id', async (request, response) => {
   try {
-      
+    const id_avaliacao = request.params.id;
+
+    const delAvaliacao = await new AvaliacaoService().delete(id_avaliacao);
+
+    return response.json(delAvaliacao);
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
