c87fbfdf1a20de18499897c0705f732e32d1d3a5
TheJamylle <jamyllemagal@gmail.com>
2020-12-08 15:04:23 -0300

    Routes projeto

--- concurso-backend/src/routes/projeto.routes.ts
+++ concurso-backend/src/routes/projeto.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import ProjetoService from '../services/ProjetoService';
 
 const projetoRouter = Router();
 
 projetoRouter.post('/', async (request, response) => {
   try {
-      
+    const { titulo, resumo, data_envio, id_area } = request.body;
+
+    const projeto = await new ProjetoService().create({ titulo, resumo, data_envio, id_area });
+
+    return response.json(projeto);
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
@@ -12,7 +17,9 @@ projetoRouter.post('/', async (request, response) => {
 
 projetoRouter.get('/', async (request, response) => {
   try {
-      
+    const projetos = await new ProjetoService().list();
+
+    return response.json(projetos);
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
@@ -20,7 +27,11 @@ projetoRouter.get('/', async (request, response) => {
 
 projetoRouter.get('/:id', async (request, response) => {
   try {
-      
+    const id_projeto = request.params.id;
+
+    const projeto = await new ProjetoService().getByID(id_projeto);
+
+    return response.json(projeto);
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
@@ -28,7 +39,13 @@ projetoRouter.get('/:id', async (request, response) => {
 
 projetoRouter.put('/:id', async (request, response) => {
   try {
-      
+    const id_projeto = request.params.id;
+
+    const { titulo, resumo, data_envio, id_area } = request.body;
+
+    const projetoUpdated = await new ProjetoService().update({ titulo, resumo, data_envio, id_area }, id_projeto);
+
+    return response.json(projetoUpdated);
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
@@ -36,7 +53,11 @@ projetoRouter.put('/:id', async (request, response) => {
 
 projetoRouter.delete('/:id', async (request, response) => {
   try {
-      
+    const id_projeto = request.params.id;
+
+    const delProjeto = await new ProjetoService().delete(id_projeto);
+
+    return response.json(delProjeto);
   } catch (error) {
     return response.status(401).json({ error: error.message });
   }
